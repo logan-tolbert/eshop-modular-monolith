6dea9c44f5f4ef9a00a6f9b2045e163c7614593a
Logan Tolbert <lodev205@gmail.com>
2025-06-26 05:48:30 -0500

    🧱 Add Product model

---
+++ Modules/Catalog/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace Catalog.Models;
+
+public class Product : Entity<Guid>
+{
+    public Guid Id {get;set;}
+    public string Name {get;set;} = string.Empty;
+    public List<string> Category {get; set;}
+    public string Description {get;set;} = string.Empty;
+    public string ImageFile {get;set;} = string.Empty;
+    public decimal Price {get;set;}
+}
